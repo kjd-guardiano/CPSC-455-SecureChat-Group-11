9129fa4109e87aa17d98720a9f32c7b207e00e91
unknown <carancijacob@csu.fullerton.edu>
2025-02-03 17:19:09 -0800

    Added chat list on sent messages

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import FLask, render_template
+from flask import Flask, render_template
 from flask_socketio import SocketIO
 
 app = Flask(__name__)
@@ -6,7 +6,7 @@ app.config['SECRET_KEY'] = 'dummy_secret_key'
 
 socketio = SocketIO(app)
 
-@app.route('/')
+@app.route('/') 
 def index():
     return render_template('index.html')
 
@@ -16,8 +16,12 @@ def handle_connect():
 
 @socketio.on('message')
 def handle_message(data):
-  print('Received message: ', data)
-  socketio.emit('response', 'Server received your message: ' + data)
+  message = data.get('message')    #sets message from dictionary
+  userID = data.get('userID')       #sets userid from dictionary
+  message1 = f"Message: '{message}'\nSent by User: {userID}" #formats message
+
+  print(message1)
+  socketio.emit('response', message1)      #sends back the message as a single string
 
 if __name__ == '__main__':
   socketio.run(app, debug=True)
