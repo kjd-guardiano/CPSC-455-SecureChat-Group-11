f717ddf448e0e5ff07d20f8e82a11cafe08edbfe
unknown <carancijacob@csu.fullerton.edu>
2025-02-03 19:26:30 -0800

    Added sent/recieved lists, and created functionality with multiple clients

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from flask_socketio import SocketIO
 
 app = Flask(__name__)
@@ -6,22 +6,36 @@ app.config['SECRET_KEY'] = 'dummy_secret_key'
 
 socketio = SocketIO(app)
 
+clients = []
+
 @app.route('/') 
 def index():
     return render_template('index.html')
 
 @socketio.on('connect')
 def handle_connect():
-  print('Client connected!')
+    sessionID = request.sid
+    clients.append(sessionID)
+    print('Client connected!')
 
 @socketio.on('message')
 def handle_message(data):
-  message = data.get('message')    #sets message from dictionary
-  userID = data.get('userID')       #sets userid from dictionary
-  message1 = f"Message: '{message}'\nSent by User: {userID}" #formats message
+    sessionID = request.sid
+    c1 = clients[0]
+    c2 = clients[1]
 
-  print(message1)
-  socketio.emit('response', message1)      #sends back the message as a single string
+    confirm = 0
+    if sessionID == c1:
+        confirm = c2
+    else:
+        confirm = c1
+
+    message = data.get('message')    #sets message from dictionary
+    userID = data.get('userID')       #sets userid from dictionary
+    message1 = f"Message: '{message}'\nSent by User: {userID}" #formats message
+
+    print(message1)
+    socketio.emit('response', message1, to=confirm)      #sends back the message as a single string
 
 if __name__ == '__main__':
   socketio.run(app, debug=True)
