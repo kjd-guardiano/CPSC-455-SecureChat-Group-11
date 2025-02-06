7b26a08be091ec56e6abd688d658818a30a8094d
unknown <carancijacob@csu.fullerton.edu>
2025-02-05 21:51:11 -0800

    Added login form and authentication file

--- app.py
+++ app.py
@@ -3,13 +3,15 @@ from flask_socketio import SocketIO
 from flask_limiter import Limiter
 from flask_limiter.util import get_remote_address
 
+import authentication
+
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'dummy_secret_key'
 
 #rate limiting link access default settings
 access_limiter = Limiter(get_remote_address,
                          app=app,
-                         default_limits=['200 per day', '20 per hour'],
+                         default_limits=['2000 per day', '2000 per hour'],
                          storage_uri="memory://")
 # [TODO] add message limiting later (need to figure out order for message limiter var creation?)
 #message_limiter = Limiter(handle_message,  # type: ignore
@@ -19,7 +21,10 @@ access_limiter = Limiter(get_remote_address,
 #socket setup
 socketio = SocketIO(app)
 
-clients = []
+clients = {
+            'Billy':(False,0), #Username: (whether or not they are logged in, sid)
+            'Sally':(False,0)
+           }
 
 @app.route('/') 
 @access_limiter.limit("10 per hour")
@@ -28,25 +33,39 @@ def index():
 
 @socketio.on('connect')
 def handle_connect():
-    sessionID = request.sid
-    clients.append(sessionID)
-    print('Client connected!')
+    print('Client sid:',request.sid, ' connected!')
+
+
+@socketio.on('authenticate')
+def authenticate(login_info):
+    username = login_info.get('username')
+    password = login_info.get('password')
+    login_sucess = -1
+    if authentication.pword_check(username,password):
+        login_sucess = 1
+        print(username,' successfully logged in!')
+        clients[username] = (True,request.sid)
+    socketio.emit('login1',login_sucess)
+
 
 @socketio.on('message')
 #@message_limiter.limit("10 per minute")
 def handle_message(data):
     sessionID = request.sid
-    c1 = clients[0]
-    c2 = clients[1]
-
     confirm = 0
-    if sessionID == c1:
-        confirm = c2
-    else:
-        confirm = c1
+    person = ''
+    for i in clients:
+        if i[2] != sessionID:
+            confirm = i[2]
+    
+    for i in clients:
+        if i[2] == sessionID:
+            person = i[0]
+            
+        
 
     message = data.get('message')    #sets message from dictionary
-    userID = data.get('userID')       #sets userid from dictionary
+    userID = person     #sets userid from dictionary
     message1 = f"Message: '{message}'\nSent by User: {userID}" #formats message
 
     print(message1)
---
+++ authentication.py
@@ -0,0 +1,32 @@
+import bcrypt
+import pwinput
+import os
+
+
+
+
+
+def pword_check(username_,password_):
+    if not os.path.exists("db.txt"):
+        with open("db.txt","w") as file:
+            pass
+        
+    username = username_
+    password = password_
+    
+    valid_login = False
+    with open('db.txt','r') as file:
+        for text in file:
+            parts = text.split()
+            if username == parts[0]:
+                stored_pass = parts[1]
+                stored_pass = stored_pass.encode(encoding='utf-8')
+                password = password.encode(encoding='utf-8')
+                if bcrypt.checkpw(password, stored_pass):
+                    valid_login = True
+                    
+    if valid_login:
+        return True
+        
+    else:
+        return False
