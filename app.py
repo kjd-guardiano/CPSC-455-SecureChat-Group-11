04424006359d31a7eac0ad6b690442fbd0857182
Katherine <kjd.guardiano@gmail.com>
2025-02-05 23:10:59 -0800

    Created basic rate limiting on messages (20 per minute), still need to only send rate limiting message to sender, not receiver. Currently prevents sending of 'real message' to receiver.

--- app.py
+++ app.py
@@ -2,6 +2,11 @@ from flask import Flask, render_template, request
 from flask_socketio import SocketIO
 from flask_limiter import Limiter
 from flask_limiter.util import get_remote_address
+import asyncio
+import math
+import time
+from contextlib import asynccontextmanager
+from limits import *
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'dummy_secret_key'
@@ -12,10 +17,33 @@ access_limiter = Limiter(get_remote_address,
                          default_limits=['200 per day', '20 per hour'],
                          storage_uri="memory://")
 # [TODO] add message limiting later (need to figure out order for message limiter var creation?)
-#message_limiter = Limiter(handle_message,  # type: ignore
-#                          app=app,
-#                          default_limits=['500 per day', '50 per hour'],
-#                          storage_uri="memory://")
+#bucket-based rate limiter (For SocketIO functions)
+class SocketIO_Limiter:
+    def __init__(self, rate, capacity) -> None:
+        self.capacity = capacity
+        self.tokens = capacity
+        self.rate = rate
+        self.last_check = time.time()
+
+    #func for limiting, takes 1 token
+    def allow_request(self, amount=1):
+        current_time = time.time()
+        passed_time = current_time - self.last_check
+        self.last_check = current_time #updates time checked
+
+        self.tokens += passed_time * self.rate
+        if self.tokens > self.capacity:
+            self.tokens = self.capacity
+
+        if self.tokens >= amount:
+            self.tokens -= amount
+            print("All good!")
+            return True
+        else:
+            print("Rate limiting enabled!")
+            return False
+
+rate_limiter = SocketIO_Limiter(1/20, 20)
 #socket setup
 socketio = SocketIO(app)
 
@@ -35,22 +63,25 @@ def handle_connect():
 @socketio.on('message')
 #@message_limiter.limit("10 per minute")
 def handle_message(data):
-    sessionID = request.sid
-    c1 = clients[0]
-    c2 = clients[1]
+    if(rate_limiter.allow_request()):
+        sessionID = request.sid
+        c1 = clients[0]
+        c2 = clients[1]
 
-    confirm = 0
-    if sessionID == c1:
-        confirm = c2
+        confirm = 0
+        if sessionID == c1:
+            confirm = c2
+        else:
+            confirm = c1
+
+        message = data.get('message')    #sets message from dictionary
+        userID = data.get('userID')       #sets userid from dictionary
+        message1 = f"Message: '{message}'\nSent by User: {userID}" #formats message
+
+        print(message1)
+        socketio.emit('response', message1, to=confirm)      #sends back the message as a single string
     else:
-        confirm = c1
-
-    message = data.get('message')    #sets message from dictionary
-    userID = data.get('userID')       #sets userid from dictionary
-    message1 = f"Message: '{message}'\nSent by User: {userID}" #formats message
-
-    print(message1)
-    socketio.emit('response', message1, to=confirm)      #sends back the message as a single string
+        socketio.emit('response', "Rate-limited! User needs to slow down!", to=confirm)
 
 if __name__ == '__main__':
   socketio.run(app, debug=True)
